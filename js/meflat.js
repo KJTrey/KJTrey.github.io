488112f4d27af40158d5320ec65f18a9968b5144
Kyle Johnson <kyle.johnson@snapiot.com>
2018-01-04 22:42:16 -0800

    moon test

--- js/meflat.js
+++ js/meflat.js
@@ -137,6 +137,39 @@ $(document).ready(function() {
 	ocean.appendChild(docFrag);
 
 	/*============================================
+	Parralax Functions
+	==============================================*/
+	$.fn.moveIt = function(){
+		var $window = $(window);
+		var instances = [];
+		
+		$(this).each(function(){
+		  instances.push(new moveItItem($(this)));
+		});
+		
+		window.addEventListener('scroll', function(){
+		  var scrollTop = $window.scrollTop();
+		  instances.forEach(function(inst){
+			inst.update(scrollTop);
+		  });
+		}, {passive: true});
+	  }
+	  
+	  var moveItItem = function(el){
+		this.el = $(el);
+		this.speed = parseInt(this.el.attr('data-scroll-speed'));
+	  };
+	  
+	  moveItItem.prototype.update = function(scrollTop){
+		this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
+	  };
+	  
+	  // Initialization
+	  $(function(){
+		$('[data-scroll-speed]').moveIt();
+	  });
+
+	/*============================================
 	Navigation Functions
 	==============================================*/
 	if ($(window).scrollTop()===0){
