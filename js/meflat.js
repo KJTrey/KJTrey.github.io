ef37316c3f85b5d3a0c4bc024a9345fae661cd72
Kyle Johnson <kyle.johnson@snapiot.com>
2018-01-07 12:40:20 -0800

    particles.json added

--- js/meflat.js
+++ js/meflat.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-	particlesJS.load('particles-js', function() {
+	particlesJS.load('particles-js','assets/particles.json', function() {
 		console.log('callback - particles.js config loaded');
 	  });
 	/*============================================
