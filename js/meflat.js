d3f204f9057265eabe7cc712248e0bd94169f562
Kyle Johnson <kyle.johnson@snapiot.com>
2018-01-07 18:00:35 -0800

    scrollmagic + slideup test

--- js/meflat.js
+++ js/meflat.js
@@ -21,9 +21,13 @@ $(document).ready(function() {
 		triggerHook: 0,
 		//offset: 50		// start this scene after scrolling for 50px
 	})
-	scene1.setPin("#who-title") // pins the element for the the scene's duration
-	scene1.setPin("#who-image") // pins the element for the the scene's duration
+	scene1.setPin("#who") // pins the element for the the scene's duration
 	scene1.addTo(controller); // assign the scene to the controller
+	scene1.on("leave", function (event) {
+		$( "#about-me" ).slideUp( "slow", function() {
+			// Animation complete.
+		});
+	});
 
 
 
