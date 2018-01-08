7907806de63edb6a79a2917f9ba3a1b8fed9e722
Kyle Johnson <kyle.johnson@snapiot.com>
2018-01-08 15:22:51 -0800

    skill bar test

--- js/meflat.js
+++ js/meflat.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
+	/*MODIFICATION START*/
+	jQuery(document).on('scroll', function(){
+		if(jQuery('html,body').scrollTop() > jQuery('#first-sec').height()){
+	/*MODIFICATION END*/
+		jQuery(".progress-bar").each(function() {
+			jQuery(this).find(".progress-content").animate({
+			width: jQuery(this).attr("data-percentage")
+			},2000);
 
+			jQuery(this).find(".progress-number-mark").animate({
+			left: jQuery(this).attr("data-percentage")
+			},{
+			duration: 2000,
+			step: function(now, fx) {
+				var data = Math.round(now);
+				jQuery(this).find(".percent").html(data + "%");
+			}
+			});
+		});
+	/*MODIFICATION START*/
+		}
+	});
+	/*MODIFICATION END*/
 
 	/*============================================
 	Particles Functions
@@ -21,9 +43,9 @@ $(document).ready(function() {
 		}, null, null, 2);
 	
    var scene1 = new ScrollMagic.Scene({
-	   triggerElement: '#what',
-	   triggerHook: 0,
-	   duration: "100%"
+	   triggerElement: '#skill',
+	   triggerHook: 1,
+	   duration: 0
    })
    scene1.setPin("#what")
    scene1.setTween(scene1TL)
