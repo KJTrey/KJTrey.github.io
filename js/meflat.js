fe7c5b64aae7618d4edab1ce0decc6075fa28fa3
Kyle Johnson <kyle.johnson@snapiot.com>
2018-01-09 11:29:02 -0800

    adjusting tween position

--- js/meflat.js
+++ js/meflat.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
 	whatTween
 		.to($("#marble-macbook-wrapper"), 3, {yPercent:70}, 0)
 		.to($("#marble-chrome-wrapper"), 3, {yPercent:95, xPercent: -150, scale:1.3}, 0)
-		.to($("#marble-iphone-wrapper"), 3, {yPercent:70, xPercent: 150, scale:1.4}, 0);
+		.to($("#marble-iphone-wrapper"), 3, {yPercent:73, xPercent: 200, scale:1.4}, 0);
 
 	var scene2 = new ScrollMagic.Scene({
 		triggerElement: '#what-wrapper',
