1a1389e73a08c7ae7fa711c3bb4054dac21ae59a
Kyle Johnson <kyle.johnson@snapiot.com>
2018-01-09 14:05:25 -0800

    revert

--- js/app.js
+++ js/app.js
@@ -48,13 +48,13 @@ $(document).ready(function() {
 	var whatTween = new TimelineMax();
 	whatTween
 		.to($("#marble-macbook-wrapper"), 1, {ease: Power1.easeInOut, yPercent:60}, 0)
-		.to($("#marble-chrome-wrapper"), 1, {ease: Power1.easeInOut, yPercent:82, left:"50%", scale:1.3}, 0)
-		.to($("#marble-iphone-wrapper"), 1, {ease: Power1.easeInOut, yPercent:62, right:"50%", scale:1.4}, 0);
+		.to($("#marble-chrome-wrapper"), 1, {ease: Power1.easeInOut, yPercent:82, xPercent: -120, scale:1.3}, 0)
+		.to($("#marble-iphone-wrapper"), 1, {ease: Power1.easeInOut, yPercent:62, xPercent: 150, scale:1.4}, 0);
 
 	var scene2 = new ScrollMagic.Scene({
 		triggerElement: '#what-wrapper',
 		triggerHook: 0.6,
-		duration: "50%"
+		duration: "40%"
 	})
 	scene2.setTween(whatTween)
 	scene2.addTo(controller);
