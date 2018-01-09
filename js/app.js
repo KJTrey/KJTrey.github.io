de55ea35e0e797844d103b639591c72f96c6b187
Kyle Johnson <kyle.johnson@snapiot.com>
2018-01-09 13:30:13 -0800

    adjusted marble devices

--- js/app.js
+++ js/app.js
@@ -48,8 +48,8 @@ $(document).ready(function() {
 	var whatTween = new TimelineMax();
 	whatTween
 		.to($("#marble-macbook-wrapper"), 1, {ease: Power1.easeInOut, yPercent:60}, 0)
-		.to($("#marble-chrome-wrapper"), 1, {ease: Power1.easeInOut, yPercent:85, xPercent: -120, scale:1.3}, 0)
-		.to($("#marble-iphone-wrapper"), 1, {ease: Power1.easeInOut, yPercent:65, xPercent: 150, scale:1.4}, 0);
+		.to($("#marble-chrome-wrapper"), 1, {ease: Power1.easeInOut, yPercent:82, xPercent: -120, scale:1.3}, 0)
+		.to($("#marble-iphone-wrapper"), 1, {ease: Power1.easeInOut, yPercent:62, xPercent: 150, scale:1.4}, 0);
 
 	var scene2 = new ScrollMagic.Scene({
 		triggerElement: '#what-wrapper',
