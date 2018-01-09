a468f29a38d93d3ffbd3cfc5c486696d5d01a630
Kyle Johnson <kyle.johnson@snapiot.com>
2018-01-09 14:31:35 -0800

    mobile specific marble positioning

--- js/app.js
+++ js/app.js
@@ -15,6 +15,11 @@ $(document).on('scroll', function(){
 /*MODIFICATION END*/
 
 $(document).ready(function() {
+	var isMobile = false;
+	if( screen.width <= 480 ) {
+		isMobile = true;
+	}
+
 	/*============================================
 	Particles Functions
 	==============================================*/
@@ -53,11 +58,17 @@ $(document).ready(function() {
 		.to($("#marble-iphone-wrapper"), 1, {ease: Power1.easeInOut, yPercent:62, xPercent: 150, scale:1.4}, 0);
 	*/
 
-	whatTween
-		.to($("#marble-macbook-wrapper"), 1, {ease: Power1.easeInOut, yPercent:60}, 0)
-		.to($("#marble-chrome-wrapper"), 1, {ease: Power1.easeInOut, yPercent:81, left:"18%", scale:1.3}, 0)
-		.to($("#marble-iphone-wrapper"), 1, {ease: Power1.easeInOut, yPercent:62, left:"87%", scale:1.4}, 0);
-
+	if(isMobile) {
+		whatTween
+			.to($("#marble-macbook-wrapper"), 1, {ease: Power1.easeInOut, yPercent:60}, 0)
+			.to($("#marble-chrome-wrapper"), 1, {ease: Power1.easeInOut, yPercent:81, left:"40%"}, 0)
+			.to($("#marble-iphone-wrapper"), 1, {ease: Power1.easeInOut, yPercent:62, left:"80%"}, 0);
+	} else {
+		whatTween
+			.to($("#marble-macbook-wrapper"), 1, {ease: Power1.easeInOut, yPercent:60}, 0)
+			.to($("#marble-chrome-wrapper"), 1, {ease: Power1.easeInOut, yPercent:81, left:"18%", scale:1.3}, 0)
+			.to($("#marble-iphone-wrapper"), 1, {ease: Power1.easeInOut, yPercent:62, left:"87%", scale:1.4}, 0);
+	}
 	var scene2 = new ScrollMagic.Scene({
 		triggerElement: '#what-wrapper',
 		triggerHook: 0.6,
