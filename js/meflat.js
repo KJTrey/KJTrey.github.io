c1e2eb3fee38a93e14f599d7fdf9a5b3d34b0e06
Kyle Johnson <kyle.johnson@snapiot.com>
2018-01-09 11:18:47 -0800

    ypercent adjust

--- js/meflat.js
+++ js/meflat.js
@@ -40,8 +40,8 @@ $(document).ready(function() {
 	var whatTween = new TimelineMax();
 	whatTween
 		.to($("#marble-macbook-wrapper"), 3, {yPercent:70}, 0)
-		.to($("#marble-chrome-wrapper"), 3, {yPercent:100, xPercent: -100, scale:1.3}, 0)
-		.to($("#marble-iphone-wrapper"), 3, {yPercent:75, xPercent: 100, scale:1.5}, 0);
+		.to($("#marble-chrome-wrapper"), 3, {yPercent:95, xPercent: -100, scale:1.3}, 0)
+		.to($("#marble-iphone-wrapper"), 3, {yPercent:70, xPercent: 100, scale:1.5}, 0);
 
 	var scene2 = new ScrollMagic.Scene({
 		triggerElement: '#what-wrapper',
