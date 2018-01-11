5873125cb153bb222fea90f50bc910fb1edcd32d
Kyle Johnson <kyle.johnson@snapiot.com>
2018-01-10 21:06:15 -0800

    hirebutton style

--- js/app.js
+++ js/app.js
@@ -45,21 +45,7 @@ $(document).ready(function() {
 		scene1.addTo(controller);
 	}
 
-	/*
 	var whatTween = new TimelineMax();
-	whatTween
-		.to($("#marble-macbook-wrapper"), 1, {ease: Power1.easeInOut, yPercent:70}, 0)
-		.to($("#marble-chrome-wrapper"), 1, {ease: Power1.easeInOut, yPercent:95, xPercent: -150, scale:1.3}, 0)
-		.to($("#marble-iphone-wrapper"), 1, {ease: Power1.easeInOut, yPercent:75, xPercent: 180, scale:1.4}, 0);
-	*/
-
-	var whatTween = new TimelineMax();
-	/*
-	whatTween
-		.to($("#marble-macbook-wrapper"), 1, {ease: Power1.easeInOut, yPercent:60}, 0)
-		.to($("#marble-chrome-wrapper"), 1, {ease: Power1.easeInOut, yPercent:82, xPercent: -120, scale:1.3}, 0)
-		.to($("#marble-iphone-wrapper"), 1, {ease: Power1.easeInOut, yPercent:62, xPercent: 150, scale:1.4}, 0);
-	*/
 
 	if(isMobile) {
 		whatTween
